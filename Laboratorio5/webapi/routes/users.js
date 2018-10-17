c1dfd376999e5c35086d77ce5e60c5d959e3bed2
Alvaro J. Ortiz P <alvar@AJOP-HP.AJOP-HP>
2018-10-16 22:55:05 -0600

    Laboratorio 6

--- Laboratorio5/webapi/routes/users.js
+++ Laboratorio5/webapi/routes/users.js
@@ -1,37 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
+var assert = require('assert');
+var url = 'mongodb://localhost:27017/Facturas';
+var db;
 
-var facturas = {"facturas":[
-    { id: 0, serie: "A", numero: "1", fecha: "01/01/2018", nit: "7654321-1", nombre: "Maria Anders", anulada: "No" },
-    { id: 1, serie: "A", numero: "2", fecha: "05/01/2018", nit: "7572413-8", nombre: "Francisco Chang", anulada: "Si" },
-    { id: 2, serie: "A", numero: "3", fecha: "02/02/2018", nit: "2035684-9", nombre: "Roland Mendel", anulada: "No" },
-    { id: 3, serie: "A", numero: "4", fecha: "27/02/2018", nit: "5256585-4", nombre: "Helen Bennett", anulada: "No" },
-    { id: 4, serie: "A", numero: "5", fecha: "08/06/2018", nit: "7582458-6", nombre: "Yoshi Tannamuri", anulada: "No" },
-    { id: 5, serie: "A", numero: "6", fecha: "07/08/2018", nit: "2563452-1", nombre: "Giovanni Rovelli", anulada: "No" }
-  ]
-};
-
-router.get('/', function(req, res, next) {
-  res.send(facturas);
+mongo.connect(url, function(err, client) {
+  if(!err) {
+    console.log("Conexion exitosa.");
+    db = client.db('Facturas');
+  }
 });
 
 router.get('/api/v1/factura/', function(req, res, next) {
-  res.send(facturas);
-});
-
-router.get('/api/v1/factura/:id', function(req, res, next) {
-  var id = req.params.id;
-  const factura = facturas.facturas.find(f => f.id == id);
-  if(!factura) {
-    res.status(404).send('factura no encontrada');
-  } else {
-    res.status(200).send(factura);
-  }
+  db.collection('facturas').find().toArray((err, result) => {
+    if (err) {
+      return console.log(err)
+    } else {
+      res.send({facturas: result})
+    }
+  })
 });
 
 router.post('/api/v1/factura/', function(req, res, next) {
   const factura = {
-    id: facturas.facturas.length + 1,
     serie: req.body.serie,
     numero: req.body.numero,
     fecha: req.body.fecha,
@@ -39,36 +32,49 @@ router.post('/api/v1/factura/', function(req, res, next) {
     nombre: req.body.nombre,
     anulada: req.body.anulada
   }
-  facturas.facturas.push(factura);
-  res.status(201).send(factura);
+  db.collection('facturas').insertOne(factura, function(err, result){
+    assert.equal(null, err);
+    if (err) {
+      return console.log(err)
+    } else {
+      console.log("Factura creada exitosamente.");
+      res.status(201).send(result);
+    }
+  });
 });
 
 router.delete('/api/v1/factura/:id', function(req, res, next) {
   var id = req.params.id;
-  const factura = facturas.facturas.find(f => f.id == id);
-  if(!factura) {
-    res.status(404).send('factura no encontrada');
-  } else {
-    const index = facturas.facturas.indexOf(factura);
-    facturas.facturas.splice(index,1);
-    res.status(204).send(factura);
-  }
+  db.collection('facturas').deleteOne({"_id": objectId(id)}, function(err, result) {
+    assert.equal(null, err);
+    if (err) {
+      return console.log(err)
+    } else {
+      console.log('Factura eliminada exitosamente.');
+      res.status(204).send(result);
+    }
+  });
 });
 
 router.put('/api/v1/factura/:id', function(req, res, next) {
   var id = req.params.id;
-  const factura = facturas.facturas.find(f => f.id == id);
-  if(!factura) {
-    res.status(404).send('factura no encontrada');
-  } else {
-    factura.serie = req.body.serie,
-    factura.numero = req.body.numero,
-    factura.fecha = req.body.fecha,
-    factura.nit = req.body.nit,
-    factura.nombre = req.body.nombre,
-    factura.anulada = req.body.anulada
-    res.status(204).send(req.factura);
+  const factura = {
+    serie: req.body.serie,
+    numero: req.body.numero,
+    fecha: req.body.fecha,
+    nit: req.body.nit,
+    nombre: req.body.nombre,
+    anulada: req.body.anulada
   }
+  db.collection('facturas').updateOne({"_id": objectId(id)}, {$set: factura}, function(err, result) {
+    assert.equal(null, err);
+    if (err) {
+      return console.log(err)
+    } else {
+      console.log('Factura modificada exitosamente.');
+      res.status(204).send(result);
+    }
+  });
 });
 
 module.exports = router;
